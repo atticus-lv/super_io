0c446b216fe318f52f23b8e54e36306f7717ac05
atticus-lv <64118610+atticus-lv@users.noreply.github.com>
2021-12-19 00:46:53 +0800

    test: 1.3.0

--- utils/asset_helper/ops_asset_helper.py
+++ utils/asset_helper/ops_asset_helper.py
@@ -47,31 +47,6 @@ class SPIO_OT_object_asset(bpy.types.Operator):
         return {"FINISHED"}
 
 
-# class SPIO_OT_material_asset(bpy.types.Operator):
-#     bl_label = 'Material Asset'
-#     bl_idname = 'spio.material_asset'
-#     bl_options = {'UNDO_GROUPED'}
-#
-#     clear:BoolProperty(name = 'Clear',default=False)
-#
-#     def execute(self, context):
-#         for obj in context.selected_objects:
-#             for slot in obj.material_slots:
-#                 mat = slot.material
-#
-#                 if not self.clear:
-#                     mat.asset_mark()
-#                     mat.asset_generate_preview()
-#                 else:
-#                     mat.asset_clear()
-#
-#         for window in bpy.context.window_manager.windows:
-#             for area in window.screen.areas:
-#                 area.tag_redraw()
-#
-#        return {"FINISHED"}
-
-
 def register():
     bpy.utils.register_class(SPIO_OT_object_asset)
     # bpy.utils.register_class(SPIO_OT_material_asset)
