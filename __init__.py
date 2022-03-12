ea3e0758a948bfada0938c7f9f08b0973cdfe196
atticus-lv <1029910278@qq.com>
2022-03-12 13:16:04 +0800

    bump version to 1.3.7

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Super IO (SPIO)",
     "author": "Atticus",
     "blender": (2, 83, 0),
-    "version": (1, 3, 6),
+    "version": (1, 3, 7),
     "category": "Import-Export",
     "support": "COMMUNITY",
     "doc_url": "https://atticus-lv.gitee.io/super_io/#/",
