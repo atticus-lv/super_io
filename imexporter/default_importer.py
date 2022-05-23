9d5836b46214b182672886b0459ab7b27a096d07
atticus-lv <1029910278@qq.com>
2022-05-24 00:47:34 +0800

    add importer lib

--- imexporter/default_importer.py
+++ imexporter/default_importer.py
@@ -31,3 +31,103 @@ def get_importer(cpp_obj_importer=True):
         im['obj'] = 'wm.obj_import'
 
     return im
+
+
+importer_lib = {
+    'DEFAULT_DAE': {
+        'bl_idname': 'wm.collada_import',
+        'name': 'Collada (.dae)',
+        'description': 'wm.collada_import',
+        'icon': 'IMPORT',
+        'number': 99,
+        'ext': 'dae'
+    },
+    'DEFAULT_ABC': {
+        'bl_idname': 'wm.alembic_import',
+        'name': 'Alembic (.abc)',
+        'description': '',
+        'icon': 'IMPORT',
+        'number': 98,
+        'ext': 'abc'
+    },
+    'DEFAULT_USD': {
+        'bl_idname': 'wm.usd_import',
+        'name': 'USD (.usd/.usda/.usdc)',
+        'description': '',
+        'icon': 'IMPORT',
+        'number': 97,
+        'ext': ['usd', 'usda', 'usdc']
+    },
+    'DEFAULT_SVG': {
+        'bl_idname': 'import_curve.svg',
+        'name': 'SVG (.svg)',
+        'description': '',
+        'icon': 'GP_SELECT_POINTS',
+        'number': 96,
+        'ext': 'svg'
+    },
+    'DEFAULT_PLY': {
+        'bl_idname': 'import_mesh.ply',
+        'name': 'Stanford (.ply)',
+        'description': '',
+        'icon': 'IMPORT',
+        'number': 95,
+        'ext': 'ply'
+    },
+    'DEFAULT_STL': {
+        'bl_idname': 'import_mesh.stl',
+        'name': 'Stl (.stl)',
+        'description': '',
+        'icon': 'IMPORT',
+        'number': 94,
+        'ext': 'stl'
+    },
+    'DEFAULT_FBX': {
+        'bl_idname': 'import_scene.fbx',
+        'name': 'FBX (.fbx)',
+        'description': '',
+        'icon': 'IMPORT',
+        'number': 93,
+        'ext': 'fbx'
+    },
+    'DEFAULT_GLTF': {
+        'bl_idname': 'import_scene.gltf',
+        'name': 'glTF 2.0 (.gltf/.glb)',
+        'description': '',
+        'icon': 'IMPORT',
+        'number': 92,
+        'ext': ['gltf', 'glb']
+    },
+    'DEFAULT_OBJ': {
+        'bl_idname': 'import_scene.obj',
+        'name': 'Wavefront (.obj)',
+        'description': '',
+        'icon': 'IMPORT',
+        'number': 91,
+        'ext': 'obj'
+    },
+    'DEFAULT_X3D': {
+        'bl_idname': 'import_scene.x3d',
+        'name': 'X3D (.x3d/.wrl)',
+        'description': '',
+        'icon': 'IMPORT',
+        'number': 90,
+        'ext': ['x3d', 'wrl']
+    },
+    'OpenVDB': {
+        'bl_idname': 'object.volume_import',
+        'name': 'OpenVDB (.vdb)',
+        'description': '',
+        'icon': 'VOLUME_DATA',
+        'number': 89,
+        'ext': 'vdb'
+    },
+    'MotionCapture': {
+        'bl_idname': 'import_anim.bvh',
+        'name': 'Motion Capture (.bvh)',
+        'description': '',
+        'icon': 'VOLUME_DATA',
+        'number': 88,
+        'ext': 'bvh'
+    }
+}
