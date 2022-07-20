370b9b5950725a57f86d4ab529750b223103418a
atticus <1029910278@qq.com>
2022-07-20 13:42:07 +0800

    bump version to 1.4.6

--- addon/importer/__init__.py
+++ addon/importer/__init__.py
@@ -1,12 +1,12 @@
 import bpy
-from . import op_import_ies, op_import_zip, ops_addon_import, op_blend_import_and_assign
+from . import op_import_ies, op_import_zip, ops_addon_import, op_blend_import_and_assign, op_import_pbr_from_url
 
 classes = (
     op_import_ies,
     op_import_zip,
     ops_addon_import,
     op_blend_import_and_assign,
-
+    op_import_pbr_from_url,
 )
 
 
